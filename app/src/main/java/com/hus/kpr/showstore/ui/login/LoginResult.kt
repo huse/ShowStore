ce85986549e1fb53344762563d7138d5dfa5fd8a
Hans <hus.kpr@gmail.com>
2021-03-04 16:37:57 -0800

    Initial commit

---
+++ app/src/main/java/com/hus/kpr/showstore/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.hus.kpr.showstore.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
